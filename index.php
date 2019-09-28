aafc7cdb3be0d4623f2f5f7ebbbaa340505681cf
Shobuj <shobujsa93@gmail.com>
2019-09-28 10:50:40 +0600

    edit page

--- index.php
+++ index.php
@@ -28,22 +28,7 @@
    ?>
 
 
-	<script type='text/javascript' charset='utf-8'>
-  window.liveSiteAsyncInit = function() {
-    LiveSite.init({
-      id : 'WI-VV3JY8FIMSCUIZP8VWGB'
-    });
-  };
-  (function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0],
-        p = 'https://',
-        r = Math.floor(new Date().getTime() / 1000000);
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = p + "widgets.vcdnita.com/assets/livesite.js?" + r;
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'livesite-jssdk'));
-</script>
+	
 
 </body>
 </html>
